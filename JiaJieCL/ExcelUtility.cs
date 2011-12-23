d8ce4e3bdacf748abf9df45096584180970e8c1a
JiaJie <jiajie0531@gmail.com>
2011-12-23 14:16:22 +0800

    some files

---
+++ JiaJieCL/ExcelUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+
+namespace JiaJieCL
+{
+    public class ExcelUtility
+    {
+        public Microsoft.Office.Interop.Excel.Application startExcel() 
+        {
+            Microsoft.Office.Interop.Excel.Application exc = new Microsoft.Office.Interop.Excel.Application();
+            if (exc == null)
+            {
+                throw new Exception("Excel无法启动");
+            }
+            return exc;
+        }
+
+    }
+}
